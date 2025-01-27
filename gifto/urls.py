49936a5c788a15bd8aba1e353ac0ed79660ee3b4
Andrea Esposito <aespositox@gmail.com>
2025-01-27 23:43:11 +0100

    Started working on UI for gift search tab

--- gifto/urls.py
+++ gifto/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from gifto.views import RandomFeedbackSurvey
+from gifto.views import Home, RandomFeedbackSurvey
 
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('rest/', include('gifto.rest.urls')),
-	path('random', RandomFeedbackSurvey.as_view(), name="random-feedback")
+	path('accounts/', include('django.contrib.auth.urls')),
+	path('random', RandomFeedbackSurvey.as_view(), name="random-feedback"),
+
+	path('home', Home.as_view(), name="home")
 ]
--- gifto/views.py
+++ gifto/views.py
@@ -2,3 +2,7 @@ from django.views.generic import View, TemplateView
 
 class RandomFeedbackSurvey(TemplateView):
   template_name = "feedback.html"
+
+
+class Home(TemplateView):
+  template_name = "home.html"
